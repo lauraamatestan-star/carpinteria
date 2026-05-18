01005851319dfb199050188a8eaecb8705045026
lauraamatestan-star <lauraamatestan@gmail.com>
2026-05-18 23:02:56 +0200

    update

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp, getApps } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDn6B_NhKoktb2jjGkCQOREZX3VCn4XA",
-  authDomain: "carpinteria-dffa2.firebaseapp.com",
-  projectId: "carpinteria-dffa2",
-  storageBucket: "carpinteria-dffa2.appspot.com",
-  messagingSenderId: "606692813667",
-  appId: "1:606692813667:web:6a12b427ea30b1e6ae0156",
-  measurementId: "G-Q7SHFHE818"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 const app = getApps().length ? getApps()[0] : initializeApp(firebaseConfig);
